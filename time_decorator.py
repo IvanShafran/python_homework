01579d3aafa5d6a0777b3b392453fa6854792a04
Ivan Shafran <vanobox07@mail.ru>
2014-04-05 21:13:23 +0400

    Change time_decorator

--- time_decorator.py
+++ time_decorator.py
@@ -1,9 +1,9 @@
 from time import time
 
 def time_decorator(user_function):
-    def user_function_with_time(*args):
+    def user_function_with_time(*args, **kwargs):
         start_time = time()
-        result = user_function(*args)
+        result = user_function(*args, **kwargs)
         print(str(time() - start_time))
         return result
 
